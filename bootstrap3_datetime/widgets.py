6cebc8ea5ffbe1137678de20a1d875f4140da8dc
areski <areski@gmail.com>
2017-11-10 10:37:03 +0100

    [feat](timepicker) add TimePicker support

--- bootstrap3_datetime/widgets.py
+++ bootstrap3_datetime/widgets.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.forms.utils import flatatt
-from django.forms.widgets import DateTimeInput
+from django.forms.widgets import DateTimeInput, TimeInput
 from django.utils import translation
 from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
@@ -71,19 +71,20 @@ class DateTimePicker(DateTimeInput):
 
     # http://momentjs.com/docs/#/parsing/string-format/
     # http://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
-    format_map = (('DDD', r'%j'),
-                  ('DD', r'%d'),
-                  ('MMMM', r'%B'),
-                  ('MMM', r'%b'),
-                  ('MM', r'%m'),
-                  ('YYYY', r'%Y'),
-                  ('YY', r'%y'),
-                  ('HH', r'%H'),
-                  ('hh', r'%I'),
-                  ('mm', r'%M'),
-                  ('ss', r'%S'),
-                  ('a', r'%p'),
-                  ('ZZ', r'%z'),
+    format_map = (
+        ('DDD', r'%j'),
+        ('DD', r'%d'),
+        ('MMMM', r'%B'),
+        ('MMM', r'%b'),
+        ('MM', r'%m'),
+        ('YYYY', r'%Y'),
+        ('YY', r'%y'),
+        ('HH', r'%H'),
+        ('hh', r'%I'),
+        ('mm', r'%M'),
+        ('ss', r'%S'),
+        ('a', r'%p'),
+        ('ZZ', r'%z'),
     )
 
     @classmethod
@@ -133,7 +134,7 @@ class DateTimePicker(DateTimeInput):
         self.div_attrs = div_attrs and div_attrs.copy() or {}
         self.icon_attrs = icon_attrs and icon_attrs.copy() or {}
         self.picker_id = self.div_attrs.get('id') or None
-        if options == False:  # datetimepicker will not be initalized only when options is False
+        if not options:  # datetimepicker will not be initalized only when options is False
             self.options = False
         else:
             self.options = options and options.copy() or {}
@@ -162,8 +163,7 @@ class DateTimePicker(DateTimeInput):
             input_attrs['value'] = force_text(self.format_value(value))
         input_attrs = dict([(key, conditional_escape(val)) for key, val in input_attrs.items()])  # python2.6 compatible
         if not self.picker_id:
-             self.picker_id = (input_attrs.get('id', '') +
-                               '_pickers').replace(' ', '_')
+            self.picker_id = (input_attrs.get('id', '') + '_pickers').replace(' ', '_')
         self.div_attrs['id'] = self.picker_id
         picker_id = conditional_escape(self.picker_id)
         div_attrs = dict(
@@ -189,3 +189,111 @@ class DateTimePicker(DateTimeInput):
             return super(DateTimePicker, self).format_value(value)
         except AttributeError:
             return super(DateTimePicker, self)._format_value(value)
+
+
+# partially copied from https://github.com/joshnroy/django-bootstrap3-datetimepicker/blob/master/bootstrap3_datetime_time/widgets.py
+# credits to @joshnroy
+# As a TimeInput
+class TimePicker(TimeInput):
+
+    class Media:
+        class JsFiles(object):
+            def __iter__(self):
+                yield 'bootstrap3_datetime/js/moment-with-locales.min.js'
+                yield 'bootstrap3_datetime/js/bootstrap-datetimepicker.min.js'
+
+        js = JsFiles()
+        css = {'all': ('bootstrap3_datetime/css/bootstrap-datetimepicker.min.css',), }
+
+    # http://momentjs.com/docs/#/parsing/string-format/
+    # http://docs.python.org/2/library/datetime.html#strftime-strptime-behavior
+    format_map = (
+        ('HH', r'%H'),
+        ('hh', r'%I'),
+        ('mm', r'%M'),
+        ('ss', r'%S'),
+        ('a', r'%p'),
+        ('ZZ', r'%z'),
+    )
+
+    @classmethod
+    def conv_datetime_format_py2js(cls, format):
+        for js, py in cls.format_map:
+            format = format.replace(py, js)
+        return format
+
+    @classmethod
+    def conv_datetime_format_js2py(cls, format):
+        for js, py in cls.format_map:
+            format = format.replace(js, py)
+        return format
+
+    html_template = '''
+        <div%(div_attrs)s>
+            <input%(input_attrs)s/>
+            <span class="input-group-addon">
+                <span%(icon_attrs)s></span>
+            </span>
+        </div>'''
+
+    js_template = '''
+        <script>
+            (function(window) {
+                var callback = function() {
+                    $(function(){$("#%(picker_id)s").datetimepicker(%(options)s);});
+                };
+                if(window.addEventListener)
+                    window.addEventListener("load", callback, false);
+                else if (window.attachEvent)
+                    window.attachEvent("onload", callback);
+                else window.onload = callback;
+            })(window);
+        </script>'''
+
+    def __init__(self, attrs=None, format=None, options=None, div_attrs=None, icon_attrs=None):
+        if not icon_attrs:
+            icon_attrs = {'class': 'glyphicon glyphicon-time'}
+        if not div_attrs:
+            div_attrs = {'class': 'input-group date'}
+        if format is None and options and options.get('format'):
+            format = self.conv_datetime_format_js2py(options.get('format'))
+        super(TimePicker, self).__init__(attrs, format)
+        if 'class' not in self.attrs:
+            self.attrs['class'] = 'form-control'
+        self.div_attrs = div_attrs and div_attrs.copy() or {}
+        self.icon_attrs = icon_attrs and icon_attrs.copy() or {}
+        self.picker_id = self.div_attrs.get('id') or None
+        if not options:  # datetimepicker will not be initalized only when options is False
+            self.options = False
+        else:
+            self.options = options and options.copy() or {}
+            lang = get_momentjs_supported_locale()
+            if lang:
+                self.options['locale'] = lang
+            if format and not self.options.get('format') and not self.attrs.get('date-format'):
+                self.options['format'] = self.conv_datetime_format_py2js(format)
+
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        input_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        if value != '':
+            # Only add the 'value' attribute if a value is non-empty.
+            input_attrs['value'] = force_text(self._format_value(value))
+        input_attrs = dict([(key, conditional_escape(val)) for key, val in input_attrs.items()])  # python2.6 compatible
+        if not self.picker_id:
+            self.picker_id = input_attrs.get('id', '') + '_picker'
+        self.div_attrs['id'] = self.picker_id
+        picker_id = conditional_escape(self.picker_id)
+        div_attrs = dict(
+            [(key, conditional_escape(val)) for key, val in self.div_attrs.items()])  # python2.6 compatible
+        icon_attrs = dict([(key, conditional_escape(val)) for key, val in self.icon_attrs.items()])
+        html = self.html_template % dict(div_attrs=flatatt(div_attrs),
+                                         input_attrs=flatatt(input_attrs),
+                                         icon_attrs=flatatt(icon_attrs))
+        if not self.options:
+            js = ''
+        else:
+            js = self.js_template % dict(picker_id=picker_id,
+                                         options=json.dumps(self.options or {}))
+        return mark_safe(force_text(html + js))
